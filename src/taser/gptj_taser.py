f8c6211846e588d6b1fe8521d6c300bc2862007b
HettyPatel <Hetpatel500@gmail.com>
2024-05-12 15:58:34 -0700

    add early middle last functionality

--- src/taser/gptj_taser.py
+++ src/taser/gptj_taser.py
@@ -34,7 +34,29 @@ class GPTJTaser(AbstractTaser):
             
         elif intervention_mode == 3:
             # early Middl Last
-            pass
+            thirds = model.config.num_hidden_layers // 3
+            if layer == "early":
+                for i in range(thirds):
+                    stacked_tensor.append(model.transformer.h[i].attn.k_proj.weight)
+                    stacked_tensor.append(model.transformer.h[i].attn.q_proj.weight)
+                    stacked_tensor.append(model.transformer.h[i].attn.v_proj.weight)
+                    stacked_tensor.append(model.transformer.h[i].attn.out_proj.weight)
+            elif layer == "middle":
+                for i in range(thirds, 2*thirds):
+                    stacked_tensor.append(model.transformer.h[i].attn.k_proj.weight)
+                    stacked_tensor.append(model.transformer.h[i].attn.q_proj.weight)
+                    stacked_tensor.append(model.transformer.h[i].attn.v_proj.weight)
+                    stacked_tensor.append(model.transformer.h[i].attn.out_proj.weight)
+                    
+            elif layer == "last":
+                for i in range(2*thirds, model.config.num_hidden_layers):
+                    stacked_tensor.append(model.transformer.h[i].attn.k_proj.weight)
+                    stacked_tensor.append(model.transformer.h[i].attn.q_proj.weight)
+                    stacked_tensor.append(model.transformer.h[i].attn.v_proj.weight)
+                    stacked_tensor.append(model.transformer.h[i].attn.out_proj.weight)
+                    
+            else:
+                raise AssertionError(f"For intervention mode 3, layer should be early, middle or last got {layer}")
         
         elif intervention_mode == 4:
             for i in range(model.config.num_hidden_layers):
@@ -47,7 +69,25 @@ class GPTJTaser(AbstractTaser):
             
         elif intervention_mode == 6:
             # fc in out early Middl Last
-            pass
+            thirds = model.config.num_hidden_layers // 3
+            if layer == "early":
+                for i in range(thirds):
+                    stacked_tensor.append(model.transformer.h[i].mlp.fc_in.weight.T)
+                    stacked_tensor.append(model.transformer.h[i].mlp.fc_out.weight)
+            elif layer == "middle":
+                for i in range(thirds, 2*thirds):
+                    stacked_tensor.append(model.transformer.h[i].mlp.fc_in.weight.T)
+                    stacked_tensor.append(model.transformer.h[i].mlp.fc_out.weight)
+                    
+            elif layer == "last":
+                for i in range(2*thirds, model.config.num_hidden_layers):
+                    stacked_tensor.append(model.transformer.h[i].mlp.fc_in.weight.T)
+                    stacked_tensor.append(model.transformer.h[i].mlp.fc_out.weight)
+                    
+            else:
+                raise AssertionError(f"For intervention mode 6, layer should be early, middle or last got {layer}")
+            
+        return torch.stack(stacked_tensor, dim=0)
         
     
     @staticmethod
@@ -76,6 +116,7 @@ class GPTJTaser(AbstractTaser):
                 edited_model.transformer.h[i].attn.out_proj.weight = torch.nn.Parameter(reconstructed_tensor[4*i+3])
         # QKVO layer at a time
         elif intervention_mode == 2:
+            layer = int(layer)
             stacked_tensor = GPTJTaser.get_stacked_tensor(edited_model, intervention_mode, layer)
             reconstructed_tensor = GPTJTaser.return_reconstructed_tensor(stacked_tensor, decomposition_type, rank)
             
@@ -100,6 +141,7 @@ class GPTJTaser(AbstractTaser):
                 edited_model.transformer.h[i].mlp.fc_out.weight = torch.nn.Parameter(reconstructed_tensor[2*i+1])
                 
         elif intervention_mode == 5:
+            layer = int(layer)
             #FC-in-out layer at a time
             stacked_tensor = GPTJTaser.get_stacked_tensor(edited_model, intervention_mode, layer)
             reconstructed_tensor = GPTJTaser.return_reconstructed_tensor(stacked_tensor, decomposition_type, rank)
