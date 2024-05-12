f9e2f367138351b52a0e3dfc008727e6e8499ae4
HettyPatel <Hetpatel500@gmail.com>
2024-05-12 13:47:42 -0700

    roberta taser class

---
+++ src/taser/roberta_taser.py
@@ -0,0 +1,131 @@
+import torch
+
+from taser.abstract_taser import AbstractTaser
+from tensorly.decomposition import parafac, tucker
+import tensorly as tl
+
+class RobertaTaser(AbstractTaser):
+    def __init__(self):
+        super().__init__()
+    
+    @staticmethod
+    def get_stacked_tensor(model, intervention_mode, layer=None):
+        """
+        Get the stacked tensor for the given intervention mode
+        :param model: model to be edited
+        :param layer: layer to be edited for intervention modes that do 1 layer at a time
+        :param intervention_mode: intervention mode (QKVO across model, layer at a time etc.)
+        :return: stacked tensor
+        """
+        stacked_tensor = []
+        if intervention_mode == 1:
+            for i in range(model.config.num_hidden_layers):
+                stacked_tensor.append(model.roberta.encoder.layer[i].attention.self.query.weight)
+                stacked_tensor.append(model.roberta.encoder.layer[i].attention.self.key.weight)
+                stacked_tensor.append(model.roberta.encoder.layer[i].attention.self.value.weight)
+                stacked_tensor.append(model.roberta.encoder.layer[i].attention.output.dense.weight)
+                
+                
+        elif intervention_mode == 2:
+            stacked_tensor.append(model.roberta.encoder.layer[layer].attention.self.query.weight)
+            stacked_tensor.append(model.roberta.encoder.layer[layer].attention.self.key.weight)
+            stacked_tensor.append(model.roberta.encoder.layer[layer].attention.self.value.weight)
+            stacked_tensor.append(model.roberta.encoder.layer[layer].attention.output.dense.weight)
+            
+            
+        elif intervention_mode == 3:
+            # early Middl Last
+            # TODO: Implement later
+            pass
+        elif intervention_mode == 4:
+            for i in range(model.config.num_hidden_layers):
+                stacked_tensor.append(model.roberta.encoder.layer[i].intermediate.dense.weight.T)
+                stacked_tensor.append(model.roberta.encoder.layer[i].output.dense.weight)
+                
+                
+        elif intervention_mode == 5:
+            stacked_tensor.append(model.roberta.encoder.layer[layer].intermediate.dense.weight.T)
+            stacked_tensor.append(model.roberta.encoder.layer[layer].output.dense.weight)
+            
+        elif intervention_mode == 6:
+            # fc in out early Middl Last
+            #TODO: Implement Later 
+            '''
+            add functionality to do early middle later layers for the tensor stacking and reconstruct then edit the model
+            '''
+            pass
+        
+        return torch.stack(stacked_tensor, dim=0)
+    
+    @staticmethod
+    def get_edited_model(model, intervention_mode, decomposition_type='cp', rank=1, layer=None):
+        
+        if intervention_mode == 1:
+            # QKVO across model
+            stacked_tensor = RobertaTaser.get_stacked_tensor(model, intervention_mode)
+            reconstructed_tensor = RobertaTaser.return_reconstructed_tensor(stacked_tensor, decomposition_type, rank)
+            
+            for i in range(model.config.num_hidden_layers):
+                model.roberta.encoder.layer[i].attention.self.query.weight = reconstructed_tensor[4*i]
+                model.roberta.encoder.layer[i].attention.self.key.weight = reconstructed_tensor[4*i+1]
+                model.roberta.encoder.layer[i].attention.self.value.weight = reconstructed_tensor[4*i+2]
+                model.roberta.encoder.layer[i].attention.output.dense.weight = reconstructed_tensor[4*i+3]
+                
+        elif intervention_mode == 2:
+            # QKVO layer at a time
+            stacked_tensor = RobertaTaser.get_stacked_tensor(model, intervention_mode, layer)
+            reconstructed_tensor = RobertaTaser.return_reconstructed_tensor(stacked_tensor, decomposition_type, rank)
+            
+            model.roberta.encoder.layer[layer].attention.self.query.weight = reconstructed_tensor[0]
+            model.roberta.encoder.layer[layer].attention.self.key.weight = reconstructed_tensor[1]
+            model.roberta.encoder.layer[layer].attention.self.value.weight = reconstructed_tensor[2]
+            model.roberta.encoder.layer[layer].attention.output.dense.weight = reconstructed_tensor[3]
+            
+        elif intervention_mode == 3:
+            # early Middl Last
+            pass
+        
+        elif intervention_mode == 4:
+            # fc in out across model
+            stacked_tensor = RobertaTaser.get_stacked_tensor(model, intervention_mode)
+            reconstructed_tensor = RobertaTaser.return_reconstructed_tensor(stacked_tensor, decomposition_type, rank)
+            
+            for i in range(model.config.num_hidden_layers):
+                model.roberta.encoder.layer[i].intermediate.dense.weight = reconstructed_tensor[2*i].T
+                model.roberta.encoder.layer[i].output.dense.weight = reconstructed_tensor[2*i+1]
+                
+        elif intervention_mode == 5:
+            # fc in out layer at a time
+            stacked_tensor = RobertaTaser.get_stacked_tensor(model, intervention_mode, layer)
+            reconstructed_tensor = RobertaTaser.return_reconstructed_tensor(stacked_tensor, decomposition_type, rank)
+            
+            model.roberta.encoder.layer[layer].intermediate.dense.weight = reconstructed_tensor[0].T
+            model.roberta.encoder.layer[layer].output.dense.weight = reconstructed_tensor[1]
+            
+        elif intervention_mode == 6:
+            # fc in out early Middl Last
+            pass
+        
+        return model
+    
+    @staticmethod
+    def return_reconstructed_tensor(tensor, rank, decomp_type):
+        """
+        return the reconstructed tensor for the given tensor with given rank and decomp_type.
+        """
+        tl.set_backend('pytorch')
+        
+        if decomp_type == 'cp':
+            tensorly_tensor = tl.tensor(tensor, device='cuda')
+            factors = parafac(tensorly_tensor, rank=rank, init='random')
+            reconstructed_tensor = tl.kruskal_to_tensor(factors)
+            
+        elif decomp_type == 'tucker':
+            tensorly_tensor = tl.tensor(tensor, device='cuda')
+            core, factors = tucker(tensorly_tensor, ranks=rank, init='random')
+            reconstructed_tensor = tl.tucker_to_tensor(core, factors)
+            
+        else:
+            raise AssertionError(f"Unhandled decomposition type {decomp_type}")
+        
+        return reconstructed_tensor
